353191f3ac2922642c3c578f97dcaeed53d543ea
ValYouW <ValYouW@gmail.com>
2015-10-08 02:06:48 +0300

    Add angular

--- pages/main/main-view-controller.js
+++ pages/main/main-view-controller.js
@@ -1,6 +1,12 @@
 /* globals document */
 var CONSTANTS = require('../../common/constants.js'),
-	ipc = require('ipc');
+	ipc = require('ipc'),
+	angular = require('angular');
+
+var ngApp = angular.module('mainWinApp', []);
+ngApp.controller('mainWinCtrl', function() {
+	this.name = 'yuval';
+});
 
 ipc.on(CONSTANTS.IPC.LOAD_FILE_LIST, function onLoadFilesRequest(files) {
 	document.getElementById('txtFiles').innerHTML = files.join();
